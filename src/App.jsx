f24f979d6b5a561299609c07a7fccdbd9cb1b3b8
amurorai203 <71173161+amurorai203@users.noreply.github.com>
2025-07-07 18:38:03 +0100

    Dynamic site

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+const visitSite = import.meta.env.VITE_SITE;
+const visitLink = visitSite + '/create-checkout-session';
 
 function App() {
   const handleCheckout = async () => {
-    const res = await fetch('/create-checkout-session', {
+    const res = await fetch(visitLink, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
